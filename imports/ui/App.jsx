a8a5c1111b5e36f9802dbe3ae6f9db99a90165cd
dudonz <edgars@dudins.lv>
2023-03-02 15:26:52 +0200

    Create basic form

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
+// Contact: name, email, imageURL
 export const App = () => (
     <div>
         <h1>My Wallet</h1>
+        <form>
+            <div>
+                <label htmlFor="name">
+                    Name:
+                </label>
+                <input id="name" type="text"/>
+            </div>
+            <div>
+                <label htmlFor="email">
+                    Email:
+                </label>
+                <input id="name" type="email"/>
+            </div>
+            <div>
+                <label htmlFor="imageURL">
+                    ImageURL:
+                </label>
+                <input id="imageURL" type="text"/>
+            </div>
+            <div>
+                <button>Save Contact</button>
+            </div>
+        </form>
     </div>
 )
