2e35c146726e0cb8bd67b4c48fdf2af32596832a
dudonz <edgars@dudins.lv>
2023-03-02 16:16:32 +0200

    Create contact saving logic

--- imports/ui/ContactForm.jsx
+++ imports/ui/ContactForm.jsx
@@ -1,25 +1,45 @@
-export const ContactForm = () => (
-    <form>
-        <div>
-            <label htmlFor="name">
-                Name:
-            </label>
-            <input id="name" type="text"/>
-        </div>
-        <div>
-            <label htmlFor="email">
-                Email:
-            </label>
-            <input id="name" type="email"/>
-        </div>
-        <div>
-            <label htmlFor="imageURL">
-                ImageURL:
-            </label>
-            <input id="imageURL" type="text"/>
-        </div>
-        <div>
-            <button>Save Contact</button>
-        </div>
-    </form>
-)
+import React, {useState} from "react"
+
+export const ContactForm = () => {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [imageURL, setImageURL] = useState()
+    const saveContact = () => {
+        console.log({name, email, imageURL})
+    }
+
+    return (
+        <form>
+            <div>
+                <label htmlFor="name">
+                    Name:
+                </label>
+                <input
+                    onChange={(e) => setName(e.target.value)}
+                    id="name"
+                    type="text"/>
+            </div>
+            <div>
+                <label htmlFor="email">
+                    Email:
+                </label>
+                <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    id="name"
+                    type="email"/>
+            </div>
+            <div>
+                <label htmlFor="imageURL">
+                    ImageURL:
+                </label>
+                <input
+                    onChange={(e) => setImageURL(e.target.value)}
+                    id="imageURL"
+                    type="text"/>
+            </div>
+            <div>
+                <button type="button" onClick={saveContact}>Save Contact</button>
+            </div>
+        </form>
+    )
+}
